024c56afd7b51d40b0f23d0244151e37783ec758
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-01-30 17:07:01 +0800

    chore: update .gitignore

--- lib/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { Expression } from 'estree';
-export declare type Decorator = {
-    expression: Expression;
-};
-export declare function getDecoratorByName(node: any, name: string): Decorator | undefined;
