369ed464bdc06e1754d9b2d12cdd9f7e9bfeba1d
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-02-09 00:28:57 +0800

    feat: add pre commit hook

---
+++ scripts/pre-commit.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const chalk = require('chalk')
+const execa = require('execa')
+
+const step = msg => console.log(chalk.cyan(msg))
+const run = (bin, args, opts = {}) => execa.sync(bin, args, { stdio: 'inherit', ...opts })
+
+readyGo()
+
+async function readyGo() {
+  const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
+
+  if (stdout) {
+    step('\nLint code...')
+    lintCode(stdout)
+  } else {
+    console.log('No changes to commit.')
+  }
+}
+
+function lintCode(stdout) {
+  let arr = []
+
+  stdout.replace(/(diff\s--git\sa\/.{1,}(\s|\n|\t))b\//g, ($0, $1) => {
+    arr.push($1.replace(/diff\s--git\sa\//, ''))
+  })
+
+  arr = arr
+    .map(item => item.replace(/\s/g, ''))
+    .filter(item => path.extname(item) === '.ts')
+    .forEach(item => run('npm', ['run', 'lint:pure', item]))
+}
