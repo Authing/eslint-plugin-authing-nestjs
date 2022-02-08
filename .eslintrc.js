349120cec83733dfbd2848bd5bb31f185da0aced
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-02-08 14:15:56 +0800

    optimize: coordinated use of 'no-unused-vars' and '@typescript-eslint/no-unused-vars'

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,9 +28,16 @@ module.exports = {
       'error',
       'never'
     ],
+    // `eslint/no-unused-vars` will check all qualified ts files, include d.ts
+    // using interface to define function types is compliant, but `eslint/no-unused-vars` will prompt for unused parameters......
+    // so set `args === none` here
+    // and leave `no-unused-vars` to `@typescript-eslint/no-unused-vars`
     "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }],
+    "@typescript-eslint/no-unused-vars": ['error'],
+    
     // https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/58605
     '@typescript-eslint/no-explicit-any': ['error'],
+    
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-interface': 'off'
   }
