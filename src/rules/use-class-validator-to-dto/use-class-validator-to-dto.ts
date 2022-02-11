4a19408a413f10576f00c32be0630cc4e03ea6c1
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-02-11 12:18:36 +0800

    Optimize 'validateDto'

--- src/rules/use-class-validator-to-dto/use-class-validator-to-dto.ts
+++ src/rules/use-class-validator-to-dto/use-class-validator-to-dto.ts
@@ -17,6 +17,57 @@ export const messages = {
     'There must be at least 2 class validators for a dto item with IsOptional'
 }
 
+function validateDto(node: ClassDeclaration, context: IContext) {
+  if (!node.id?.name) {
+    return
+  }
+
+  let config = bodyParamMap.get(node.id.name) || postClassMap.get(node.id.name)
+
+  if (!config) {
+    return
+  }
+
+  if (typeof config === 'boolean') {
+    config = { node }
+  }
+
+  node.body.body.forEach((nodeItem: MethodDefinition | PropertyDefinition) => {
+    if (!nodeItem.decorators?.length) {
+      context.report({
+        node: config.node,
+        messageId: 'invalidDtoClassValidator'
+      })
+    } else if (
+      nodeItem.decorators.find(
+        decorator => decorator.expression.callee.name === 'IsOptional'
+      ) &&
+      nodeItem.decorators.length < 2
+    ) {
+      context.report({
+        node: config.node,
+        messageId: 'invalidDtoClassValidatorLength'
+      })
+    } else if (
+      nodeItem.decorators.find(
+        decorator => decorator.expression.callee.name === 'ValidateNested'
+      )
+    ) {
+      const _typeNname: string | undefined =
+        nodeItem?.typeAnnotation?.typeAnnotation?.typeName?.name
+      if (_typeNname) {
+        if (preClassMap.has(_typeNname)) {
+          bodyParamMap.set(_typeNname, preClassMap.get(_typeNname))
+          validateDto(preClassMap.get(_typeNname).node, context)
+          preClassMap.delete(_typeNname)
+        } else {
+          postClassMap.set(_typeNname, true)
+        }
+      }
+    }
+  })
+}
+
 export const useClassValidatorToDto = {
   meta: {
     messages
@@ -34,62 +85,7 @@ export const useClassValidatorToDto = {
           })
         }
 
-        validateFn(node)
-
-        function validateFn(node: ClassDeclaration) {
-          if (!node.id?.name) {
-            return
-          }
-
-          let config =
-            bodyParamMap.get(node.id.name) || postClassMap.get(node.id.name)
-
-          if (!config) {
-            return
-          }
-
-          if (typeof config === 'boolean') {
-            config = { node }
-          }
-
-          node.body.body.forEach(
-            (nodeItem: MethodDefinition | PropertyDefinition) => {
-              if (!nodeItem.decorators?.length) {
-                context.report({
-                  node: config.node,
-                  messageId: 'invalidDtoClassValidator'
-                })
-              } else if (
-                nodeItem.decorators.find(
-                  decorator => decorator.expression.callee.name === 'IsOptional'
-                ) &&
-                nodeItem.decorators.length < 2
-              ) {
-                context.report({
-                  node: config.node,
-                  messageId: 'invalidDtoClassValidatorLength'
-                })
-              } else if (
-                nodeItem.decorators.find(
-                  decorator =>
-                    decorator.expression.callee.name === 'ValidateNested'
-                )
-              ) {
-                const _typeNname: string | undefined =
-                  nodeItem?.typeAnnotation?.typeAnnotation?.typeName?.name
-                if (_typeNname) {
-                  if (preClassMap.has(_typeNname)) {
-                    bodyParamMap.set(_typeNname, preClassMap.get(_typeNname))
-                    validateFn(preClassMap.get(_typeNname).node)
-                    preClassMap.delete(_typeNname)
-                  } else {
-                    postClassMap.set(_typeNname, true)
-                  }
-                }
-              }
-            }
-          )
-        }
+        validateDto(node, context)
       },
       MethodDefinition: (node: MethodDefinition) => {
         const bodyParam: Pattern | undefined = node.value.params.find(p =>
