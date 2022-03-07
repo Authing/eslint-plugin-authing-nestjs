83f4e145fcf98e0fa3ceb62455e2e7d544be8680
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-03-07 20:36:40 +0800

    Optimize: delete duplicate rule

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import { forbidBodyParameters } from './rules/forbid-body-parameters/forbid-body
 import { forbidReadBodyFromReq } from './rules/forbid-read-body-from-req/forbid-read-body-from-req'
 import { useClassAsTypeInMethodOfController } from './rules/use-class-as-type-in-method-of-controller/use-class-as-type-in-method-of-controller'
 import { useClassValidatorToDto } from './rules/use-class-validator-to-dto/use-class-validator-to-dto'
-import { useClassValidatorToNestedDto } from './rules/use-class-validator-to-dto/use-class-validator-to-nested-dto'
 
 export const rules = {
   forbid_body_parameters: forbidBodyParameters,
   forbid_read_body_from_req: forbidReadBodyFromReq,
   use_class_as_type_in_method_of_controller: useClassAsTypeInMethodOfController,
-  use_class_validator_to_dto: useClassValidatorToDto,
-  use_class_validator_to_nested_dto: useClassValidatorToNestedDto
+  use_class_validator_to_dto: useClassValidatorToDto
 }
 
 export const configs = {
@@ -18,8 +16,7 @@ export const configs = {
       'authing-nestjs/forbid_body_parameters': 2,
       'authing-nestjs/forbid_read_body_from_req': 2,
       'authing-nestjs/use_class_as_type_in_method_of_controller': 2,
-      'authing-nestjs/use_class_validator_to_dto': 2,
-      'authing-nestjs/use_class_validator_to_nested_dto': 2
+      'authing-nestjs/use_class_validator_to_dto': 2
     }
   }
 }
